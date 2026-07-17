1e30138ac96fa1739c93c6ec7d2362f785dca482
hawkeye <git-asus@nowork.uk>
2026-07-17 16:00:28 +0100

    Coordinate wrapping is silly sometimes

--- src/main/java/uk/pilk/snek/models/Board.java
+++ src/main/java/uk/pilk/snek/models/Board.java
@@ -82,7 +82,7 @@ public class Board {
         }
 
         temp = headPos + 1;
-        if (headPos / width + 1 != width) {
+        if (headPos % width != 0) {
             temp = grid[temp].getWeight();
             if (favourWeight < temp) {
                 favourWeight = temp;
@@ -100,7 +100,7 @@ public class Board {
         }
 
         temp = headPos - 1;
-        if (temp % width != 0) {
+        if (temp % width != width) {
             temp = grid[temp].getWeight();
             if (favourWeight < temp) {
                 favoured = "left";
