342ed81a292924de0949011865df3b2ab01ebb6e
hawkeye <git-asus@nowork.uk>
2026-07-17 12:02:42 +0100

    Bare min

---
+++ src/main/java/uk/pilk/snek/Board.java
@@ -0,0 +1,18 @@
+package uk.pilk.snek;
+
+public class Board {
+
+    private int height, width;
+
+    private Tile[] tiles;
+    private int[] food;
+
+    public Board(int x, int y){
+        tiles = new Tile[x*y];
+    }
+
+    private int pointToBoard(int x, int y){
+        return
+    }
+
+}
---
+++ src/main/java/uk/pilk/snek/Snek.java
@@ -0,0 +1,8 @@
+package uk.pilk.snek;
+
+public class Snek {
+
+    private String id;
+    private String name;
+    private int health;
+}
