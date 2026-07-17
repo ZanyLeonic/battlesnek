e1999f9a81622526c7b39ab07f8bf659bad23dd2
hawkeye <git-asus@nowork.uk>
2026-07-17 12:09:54 +0100

    1D array conversions

--- src/main/java/uk/pilk/snek/models/Board.java
+++ src/main/java/uk/pilk/snek/models/Board.java
@@ -13,9 +13,23 @@ public class Board {
         tiles = new Tile[x * y];
     }
 
-    private int pointToBoard(int x, int y){
-        return 1;
+    /**
+     * Convert coordinates {x, y} to board pos
+     * @param x
+     * @param y
+     * @return board array pos
+     */
+    private int pointToBoard(int x, int y) {
+        return x + y * width;
+    }
 
+    /**
+     * Don't call unless you NEED this
+     * @param pos board pos
+     * @return Converted to coordinates {x, y}
+     */
+    private int[] boardToPoint(int pos) {
+        return new int[]{pos % width, pos / width};
     }
 
 }
