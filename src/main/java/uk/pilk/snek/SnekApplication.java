4910ef14dd46720425d151c6cf4f86b079ad9dab
Leo Durrant <poweredbykurisu@hotmail.com>
2026-07-17 12:30:55 +0200

    Initial Commit

---
+++ src/main/java/uk/pilk/snek/SnekApplication.java
@@ -0,0 +1,13 @@
+package uk.pilk.snek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/pilk/snek/SnekApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.pilk.snek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
